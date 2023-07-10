a68bf3bd60af30d863a7da12ab03242fd111bc22
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-07-10 16:59:24 +0300

    index.js add function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import getImages from './js/search-api';
+import { getImages, onFetchError } from './js/search-api';
+
+const inputRequest = document.querySelector('input');
+const btnSearch = document.querySelector('button');
+console.log(btnSearch);
+
+btnSearch.addEventListener('submit', onBtnSubmit);
+inputRequest.addEventListener('input', onRequestInput);
+
+function onBtnSubmit(evt) {
+  evt.preventDefault();
+}
+
+function onRequestInput(evt) {
+  evt.preventDefault();
+  const inputText = evt.currentTarget.value;
+  console.log(inputText);
+}
--- src/js/search-api.js
+++ src/js/search-api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const BASE_URL = `https://pixabay.com/api/`;
 const API_KEY = '38162173-e7189c612242127d8d754fc70';
@@ -7,6 +8,19 @@ const getImages = () =>
   axios.get(
     `${BASE_URL}?key=${API_KEY}&image_type=photo&q=cat&orientation=horizontal&safesearch=true&q=cat`
   );
-getImades().then(console.log).catch(console.warn);
 
-export { getImages };
+getImages().then(console.log).catch(onFetchError);
+
+function onFetchError(error) {
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!',
+    {
+      position: 'center-center',
+      timeout: 5000,
+      width: '400px',
+      fontSize: '24px',
+    }
+  );
+}
+
+export { getImages, onFetchError };

9b6c8669e5710d428312115fbf6622f5ed704d91
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-07-10 13:55:38 +0300

    1

--- src/js/search-api.js
+++ src/js/search-api.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const BASE_URL = `https://pixabay.com/api/`;
 const API_KEY = '38162173-e7189c612242127d8d754fc70';
 
-const getImades = () =>
-  axios.get(
-    `${BASE_URL}?key=${API_KEY}&image_type=photo&q=cat&orientation=horizontal&safesearch=true&q=cat`
-  );
-getImades().then(console.log).catch(console.warn);
+// const getImades = () =>
+//   axios.get(
+//     `${BASE_URL}?key=${API_KEY}&image_type=photo&q=cat&orientation=horizontal&safesearch=true&q=cat`
+//   );
+// getImades().then(console.log).catch(console.warn);
 
-export { getImades };
+// export { getImades };
